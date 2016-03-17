bc64669e6cc5cc9e4aa683eaf45af657a402db87
Chung Phuoc <ohcphuoc@lhv.vn>
2016-03-17 11:22:32 +0700

    #add unit test for the module

--- program.rb
+++ program.rb
@@ -3,4 +3,5 @@ require_relative "dollar_exchange"
 include DollarExchange
 puts 'Input the dollar value:'
 dollar = gets.strip
-dollar_break_down(dollar.to_f).map { |k, v| puts "#{k}: #{v}" }
+dollar = dollar.gsub(',', '.')
+dollar_break_down(dollar.to_f).delete_if { |k, v| v.to_i == 0 }.map { |k, v| puts "#{k}: #{v}" }
---
+++ test/dollar_exchange_test.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "../dollar_exchange"
+include DollarExchange
+
+class TestDollarExchange < Minitest::Test
+  def test_dollar_break_down
+    assert_equal({"quarters" => 20, "dimes" => 2, "nickels" => 0, "cents" => 3}, dollar_break_down(5.23))
+  end
+
+end
