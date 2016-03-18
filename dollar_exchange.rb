0e893005633e90a5113c4f10a6a1d4d0c5eafd05
Chung Phuoc <ohcphuoc@lhv.vn>
2016-03-18 08:47:29 +0700

    #optimize the code

--- dollar_exchange.rb
+++ dollar_exchange.rb
@@ -2,22 +2,22 @@ module DollarExchange
   def dollar_break_down(dollar)
     result = {}
     total_cents = (dollar * 100).to_i
-    ["quarters", "dimes", "nickels", "cents"].each do |denomination|
+    [:quarters, :dimes, :nickels, :cents].each do |denomination|
       case denomination
-      when "quarters"
+      when :quarters
         quarters = total_cents / 25
-        result["#{denomination}"] = quarters
+        result[denomination] = quarters
         total_cents -= quarters * 25
-      when "dimes"
+      when :dimes
         dimes = total_cents / 10
-        result["#{denomination}"] = dimes
+        result[denomination] = dimes
         total_cents -= dimes * 10
-      when "nickels"
+      when :nickels
         nickels = total_cents / 5
-        result["#{denomination}"] = nickels
+        result[denomination] = nickels
         total_cents -= nickels * 5
-      when "cents"
-        result["#{denomination}"] = total_cents
+      when :cents
+        result[denomination] = total_cents
       end
     end
     result
--- program.rb
+++ program.rb
@@ -4,4 +4,4 @@ include DollarExchange
 puts 'Input the dollar value:'
 dollar = gets.strip
 dollar = dollar.gsub(',', '.')
-dollar_break_down(dollar.to_f).delete_if { |k, v| v.to_i == 0 }.map { |k, v| puts "#{k}: #{v}" }
+dollar_break_down(dollar.to_f).delete_if { |k, v| v.to_i == 0 }.map { |k, v| puts "#{v} #{k.capitalize} " }
